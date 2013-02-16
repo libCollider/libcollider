777c3111830bcc90cc55e0e46d763f43319e13c8
Eric Hamdan <erichamdan@gmail.com>
2013-02-16 15:55:19 -0800

    added scsyndef for bandrejected white noise with mouse control for filter, updated Buffer_Test

--- src/tests/Buffer_Test.cpp
+++ src/tests/Buffer_Test.cpp
@@ -11,23 +11,13 @@ int main()
 {
   Client_Server cs("Server");
   Buffer b(cs._nextBufferNum());
-  b._readSoundFile(cs, "/Users/administrator/Desktop/cheesecake.aiff");
-
+  b._readSoundFile(cs, "/Users/administrator/Desktop/hat.wav");
   std::map<std::string, float> sArgs;
   sArgs["bufnum"] = b._getBufNum();
+  sArgs["stutterRate"] = 7;
+  sArgs["rate"] = 1;
   Synth s(cs, "SoundFile_Stutter_Loop_Stereo", cs._nextNodeId(), sArgs);
   sleep(24);
-     
-  //Stress test - read soundfile into new buffer as fast as possible then free - 100 times
-  //while sound created above plays continuously
- /* int count = 0;
-  while(count < 100)
-  {
-    Buffer d(cs._nextBufferNum());
-    d._readSoundFile(cs, "/Users/administrator/Desktop/bullrider.aiff");
-    d._free(cs);
-    ++count;
-  }*/
 
   s._free(cs);
   b._free(cs);
