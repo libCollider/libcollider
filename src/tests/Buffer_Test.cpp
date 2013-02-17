5d1ff78d6782d43ee2813b7bafe25b48f25d1ba2
Eric Hamdan <erichamdan@gmail.com>
2013-02-17 00:56:41 -0800

    update to Buffer_Test, user can set the soundfile to test

--- src/tests/Buffer_Test.cpp
+++ src/tests/Buffer_Test.cpp
@@ -9,11 +9,21 @@ using namespace ColliderPlusPlus;
 
 #define SYNTH_DIR "/Users/administrator/Documents/gitprojects/ColliderPlusPlus/synthdefs/"
 
-int main()
+int main(int argc, char* argv[])
 {
+  std::string soundfile;
+
+  if(argc != 2)
+  {
+    std::cerr << "Usage: Buffer_Test <Soundfile>" << std::endl;
+    return 1;
+  }
+
+  soundfile = argv[1];  
+  
   Client_Server cs("Server", SYNTH_DIR);
   Buffer b(cs._nextBufferNum());
-  b._readSoundFile(cs, "/Users/administrator/Desktop/cheesecake.aiff");
+  b._readSoundFile(cs, soundfile);
   std::map<std::string, float> sArgs;
   sArgs["bufnum"] = b._getBufNum();
   sArgs["stutterRate"] = 7;
