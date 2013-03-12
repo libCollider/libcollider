a6f49a1a4c119b4dfa4eebc46dd7ac57553f9fb1
Eric Hamdan <erichamdan@gmail.com>
2013-03-11 19:14:29 -0700

    update

--- include/ColliderPlusPlus.hpp
+++ include/ColliderPlusPlus.hpp
@@ -1,5 +1,5 @@
 /// @file ColliderPlusPlus.hpp
-/// @brief Header file for ColliderPlusPlus.hpp
+/// @brief A convenience header when using the library
 /// @author Eric Hamdan erichamdan@gmail.com
 #ifndef COLLIDER_PLUS_PLUS
 #define COLLIDER_PLUS_PLUS
