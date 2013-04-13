a7937d78bd1809ebfdb91cbfaa8b9c671e84d5bd
Eric Hamdan <erichamdan@gmail.com>
2013-04-13 08:06:47 -0700

    updated docs

--- doc/html/search/all_73.js
+++ doc/html/search/all_73.js
@@ -2,10 +2,12 @@ var searchData=
 [
   ['set',['set',['../classColliderPlusPlus_1_1Node.html#af584b2b7f4c17495f19e943e3f9a9a2c',1,'ColliderPlusPlus::Node']]],
   ['setbufnum',['setBufNum',['../classColliderPlusPlus_1_1Buffer.html#a860869166e3ded181813158b8ba9159b',1,'ColliderPlusPlus::Buffer']]],
+  ['setgain',['setGain',['../classColliderPlusPlus_1_1Sound.html#ab64f46659f59a6d7b7bc206cca6402cf',1,'ColliderPlusPlus::Sound']]],
   ['setn',['setn',['../classColliderPlusPlus_1_1Node.html#a898b7636dbf6c20c331625e7df397e1e',1,'ColliderPlusPlus::Node']]],
   ['setnodecontrols',['setNodeControls',['../classColliderPlusPlus_1_1Client__Server.html#afbcd572869652e6e7ec7af430b70f2f7',1,'ColliderPlusPlus::Client_Server']]],
   ['setnumchans',['setNumChans',['../classColliderPlusPlus_1_1Buffer.html#acfee7fb4d7e5ff2dadf2aae85abd6ff6',1,'ColliderPlusPlus::Buffer']]],
   ['setnumframes',['setNumFrames',['../classColliderPlusPlus_1_1Buffer.html#a4326fdc6e8e7e988adf7188c8cfc5fc1',1,'ColliderPlusPlus::Buffer']]],
+  ['setrate',['setRate',['../classColliderPlusPlus_1_1Sound.html#a5bd234602a61929747697677d972fc30',1,'ColliderPlusPlus::Sound']]],
   ['setsamprate',['setSampRate',['../classColliderPlusPlus_1_1Buffer.html#a028e5dc76e6b54faf9ef0a3c9eeb2ae1',1,'ColliderPlusPlus::Buffer']]],
   ['sound',['Sound',['../classColliderPlusPlus_1_1Sound.html',1,'ColliderPlusPlus']]],
   ['sound',['Sound',['../classColliderPlusPlus_1_1Sound.html#ac1e87781b6bb69d6a250d2eaab360994',1,'ColliderPlusPlus::Sound']]],
@@ -13,6 +15,6 @@ var searchData=
   ['status',['status',['../classColliderPlusPlus_1_1Client__Server.html#a1a0642fa152a9c1ad5a5ca16f31aa6e1',1,'ColliderPlusPlus::Client_Server']]],
   ['stop',['stop',['../classColliderPlusPlus_1_1Node.html#a44da500743469ad8a4729967d1d8e2b4',1,'ColliderPlusPlus::Node::stop()'],['../classColliderPlusPlus_1_1Sound.html#a7010e6bcdd47d51b359c364883574a5d',1,'ColliderPlusPlus::Sound::stop()']]],
   ['sync',['sync',['../classColliderPlusPlus_1_1Buffer.html#af6efd69eef95bf0202498acc256e7f3f',1,'ColliderPlusPlus::Buffer']]],
-  ['synth',['Synth',['../classColliderPlusPlus_1_1Synth.html',1,'ColliderPlusPlus']]],
-  ['synth',['Synth',['../classColliderPlusPlus_1_1Synth.html#a95d3df49d21a4c6844089ae7641a1be4',1,'ColliderPlusPlus::Synth::Synth(Client_Server *other, const std::string &amp;defName, int id_, int initAction=0, int addAction=TO_HEAD, int target=DEFAULT_GROUP)'],['../classColliderPlusPlus_1_1Synth.html#a6b1104ec5673c05ebe9d7804a1d85af8',1,'ColliderPlusPlus::Synth::Synth(Client_Server *other, const std::string &amp;defName, int id_, std::map&lt; std::string, float &gt; &amp;args, int initAction=0, int addAction=TO_HEAD, int target=DEFAULT_GROUP)']]]
+  ['synth',['Synth',['../classColliderPlusPlus_1_1Synth.html#a95d3df49d21a4c6844089ae7641a1be4',1,'ColliderPlusPlus::Synth::Synth(Client_Server *other, const std::string &amp;defName, int id_, int initAction=0, int addAction=TO_HEAD, int target=DEFAULT_GROUP)'],['../classColliderPlusPlus_1_1Synth.html#a6b1104ec5673c05ebe9d7804a1d85af8',1,'ColliderPlusPlus::Synth::Synth(Client_Server *other, const std::string &amp;defName, int id_, std::map&lt; std::string, float &gt; &amp;args, int initAction=0, int addAction=TO_HEAD, int target=DEFAULT_GROUP)']]],
+  ['synth',['Synth',['../classColliderPlusPlus_1_1Synth.html',1,'ColliderPlusPlus']]]
 ];
--- doc/html/search/functions_73.js
+++ doc/html/search/functions_73.js
@@ -2,10 +2,12 @@ var searchData=
 [
   ['set',['set',['../classColliderPlusPlus_1_1Node.html#af584b2b7f4c17495f19e943e3f9a9a2c',1,'ColliderPlusPlus::Node']]],
   ['setbufnum',['setBufNum',['../classColliderPlusPlus_1_1Buffer.html#a860869166e3ded181813158b8ba9159b',1,'ColliderPlusPlus::Buffer']]],
+  ['setgain',['setGain',['../classColliderPlusPlus_1_1Sound.html#ab64f46659f59a6d7b7bc206cca6402cf',1,'ColliderPlusPlus::Sound']]],
   ['setn',['setn',['../classColliderPlusPlus_1_1Node.html#a898b7636dbf6c20c331625e7df397e1e',1,'ColliderPlusPlus::Node']]],
   ['setnodecontrols',['setNodeControls',['../classColliderPlusPlus_1_1Client__Server.html#afbcd572869652e6e7ec7af430b70f2f7',1,'ColliderPlusPlus::Client_Server']]],
   ['setnumchans',['setNumChans',['../classColliderPlusPlus_1_1Buffer.html#acfee7fb4d7e5ff2dadf2aae85abd6ff6',1,'ColliderPlusPlus::Buffer']]],
   ['setnumframes',['setNumFrames',['../classColliderPlusPlus_1_1Buffer.html#a4326fdc6e8e7e988adf7188c8cfc5fc1',1,'ColliderPlusPlus::Buffer']]],
+  ['setrate',['setRate',['../classColliderPlusPlus_1_1Sound.html#a5bd234602a61929747697677d972fc30',1,'ColliderPlusPlus::Sound']]],
   ['setsamprate',['setSampRate',['../classColliderPlusPlus_1_1Buffer.html#a028e5dc76e6b54faf9ef0a3c9eeb2ae1',1,'ColliderPlusPlus::Buffer']]],
   ['sound',['Sound',['../classColliderPlusPlus_1_1Sound.html#ac1e87781b6bb69d6a250d2eaab360994',1,'ColliderPlusPlus::Sound']]],
   ['status',['status',['../classColliderPlusPlus_1_1Client__Server.html#a1a0642fa152a9c1ad5a5ca16f31aa6e1',1,'ColliderPlusPlus::Client_Server']]],
